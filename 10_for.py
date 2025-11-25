affa58d5a53a08d71e0d2408f97228a8888a7b97
DESKTOP-7RMRFTU\admin <study.todair@gmail.com>
2025-11-25 12:27:12 +0900

    파이썬 - for, 컨프리헨션

--- 10_for.py
+++ 10_for.py
@@ -109,3 +109,211 @@ for i in range(5) :
     break
 else :
     print('break 안만남')
+
+
+print('#'*20)
+#####################
+# 리스트 컴프리헨션 comprehension
+
+for i in range(1, 10) :
+    print(i)
+
+
+a4 = '''
+    *
+   ***
+  *****
+ *******
+'''
+a5 = '''
+    *
+   ***
+  *****
+ *******
+*********
+'''
+print(a5)
+
+# 1,2,...,9 값을 가지는 리스트를 쉽게 만들어보자 
+a = [1,2,3]
+'''
+    리스트 선언 a
+    반복 1~9 만큼
+        a에 하나씩 추가
+    a 출력해서 확인
+'''
+a = []
+for i in range(1, 10) :
+    a.append(i)
+print(a)
+
+print('-'*20)
+a = [ i for i in range(1, 10)  ]
+print(a)
+
+# 17의 배수 5개
+b = []
+for i in range(1, 6) :
+    b.append(17 * i)
+
+b = [ (i * 17) for i in range(1, 6)  ]
+
+for i in range(17, (17*5)+1, 17) :
+    b.append(i)
+b = [ i for i in range(17, (17*5)+1, 17) ]
+
+c = []
+for i in range(1, 10) :
+    if i % 3 == 0 :
+        c.append(i)
+
+for i in range(3, 10, 3) :
+    c.append(i)
+
+c = [i for i in range(1, 10) if i % 3 == 0 ]
+print(c)
+
+c = []
+for i in range(1, 10) :
+    if (i % 3 == 0) or (i % 5 == 0):
+        c.append(i)
+c = [ i for i in range(1, 10) if (i % 3 == 0) or (i % 5 == 0) ] 
+
+# 1~9까지에서
+# 홀수는 그대로
+# 짝수는 2배 인 배열을 만들자
+for i in range(1, 10) :
+    # 1단계
+    if i % 2 == 0 :
+        c.append(i * 2)
+    else :
+        c.append(i)
+
+    # 2단계
+    if i % 2 == 0 :
+        value = i * 2
+    else :
+        value = i
+
+    # 3단계
+    value = i*2 if i % 2 == 0 else i
+    c.append(value)
+
+    # 4단계
+    c.append(i*2 if i % 2 == 0 else i)
+
+        
+c = [ i*2 if i % 2 == 0 else i for i in range(1, 10) ]
+
+print('-'*20)
+## 구구단
+d = []
+for i in range(2, 10) :     # 바깥쪽
+    for j in range(1, 10) : # 안쪽
+        d.append(f'{i}x{j}={i*j}')
+print(d)
+
+# 안쪽부터
+e = [ [ f'{i}x{j}={i*j}' for j in range(1, 10) ] for i in range(2, 10) ]
+# 이건 의도와 다르게 2차원 배열이 된다
+print('--- e ---')
+print(e)
+
+# 그렇다면
+f = [f'{i}x{j}={i*j}' for i in range(2, 10) 
+                      for j in range(1, 10)]
+print('--- f ---')
+print(f)
+
+# 딕셔너리 컴프리헨션
+a = {
+    '0': 0,
+    '1': 1,
+    '2': 4,
+    '3': 9,
+    '4': 16
+}
+a = {}
+for i in range(5) :
+    if i%2 == 0 :
+        a[i] = i ** 2
+
+a = {  i : i**2 for i in range(5) if i%2 == 0 }
+print(a)
+
+print('-'*20)
+a = [11,12,13,14]
+for i, data in enumerate(a) :
+    print(i, data)
+print('-'*20)
+for i, data in enumerate(a, 2) :
+    print(i, data)
+
+print('-'*20)
+a = [10,20,30]
+b = [6,7,8,9]
+c = zip(a, b)
+print(c)
+d = list(zip(a, b))
+print(d)
+
+lang = 'Kor'
+kr = ['소개', '채용']
+en = ['About', 'Recruit']
+
+# '소개'를 출력
+if lang == 'Kor' :
+    print(kr[0])
+elif lang == 'Eng' :
+    print(en[0])
+
+i18n = []
+if lang == 'Kor' :
+    i18n = kr
+elif lang == 'Eng' :
+    i18n = en
+
+html = f'''
+    <span>{i18n[0]}</span> <span>{i18n[1]}</span>
+'''
+
+d_kr = {
+    'common.header.about' : '소개'
+}
+
+d_en = {
+    'common.header.about' : 'About'
+}
+
+# if를 통해서
+i18n = d_kr
+
+html = f'''
+    <span>{i18n['common.header.about']}</span>
+'''
+
+
+'''
+문제1
+1~n 까지 짝수의 합을 구하기
+
+문제2
+1~n 까지 숫자를 3개씩 옆으로 찍기
+
+문제3
++++*---
+++***--
++*****-
+*******
+
+문제4
+주사위 2개를 굴려서 중복을 제거한 경우의 수
+[1, 2] [2, 1] : 중복이라서 하나로 보기
+
+문제5
+키오스크 처럼 
+'1: 아아, 2: 라떼, x: 종료'
+'1' 입력 시 "아아" 출력
+'x'가 입력되기 전까지 무한 반복
+'''
+
